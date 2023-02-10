01e5e53a29707a69eb3f841baf98028a5cddf530
PobbleBobble <PBDKiely@gmail.com>
2023-02-10 20:55:42 +0000

    Add files via upload

---
+++ graphics/portraits/CustomPortraitsAddon.asm
@@ -0,0 +1,16 @@
+* = $4E0000
+.logical $670000
+
+	
+	
+	g4bppcPortraitSara .binary "sprites/Sara.4bpp.comp"
+	g4bppcPortraitCohen .binary "sprites/Cohen.4bpp.comp"
+	g4bppcPortraitTravant .binary "sprites/Travant.4bpp.comp"
+	g4bppcPortraitCoulter .binary "sprites/Coulter.4bpp.comp"
+	g4bppcPortraitBeatrice .binary "sprites/Beatrice.4bpp.comp"
+	g4bppcPortraitGunnar
+	;Gunnar ain't done yet baby! .binary "sprites/Gunnar.4bpp.comp"
+	
+	
+.here
+	
