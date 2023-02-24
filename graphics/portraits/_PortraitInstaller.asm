351fc63c1c7987bec4da022d387ec599a58d43ec
PobbleBobble <PBDKiely@gmail.com>
2023-02-24 23:26:34 +0000

    cohengunnartanya p4

--- graphics/portraits/_PortraitInstaller.asm
+++ graphics/portraits/_PortraitInstaller.asm
@@ -447,8 +447,7 @@
 	g4bppcPortraitCoulter .binary "sprites/Coulter.4bpp.comp"
 	g4bppcPortraitBeatrice .binary "sprites/Beatrice.4bpp.comp"
 	g4bppcPortraitFilat .binary "sprites/Filat.4bpp.comp"
-	;g4bppcPortraitGunnar
-	;Gunnar ain't done yet baby! .binary "sprites/Gunnar.4bpp.comp"
+	g4bppcPortraitGunnar .binary "sprites/Gunnar.4bpp.comp"
 	g4bppcPortraitEAFE34 .binary "sprites/PortraitEAFE34.4bpp.comp"
 	g4bppcPortraitEBFD2F .binary "sprites/PortraitEBFD2F.4bpp.comp"
 	g4bppcPortraitECFE67 .binary "sprites/PortraitECFE67.4bpp.comp"
