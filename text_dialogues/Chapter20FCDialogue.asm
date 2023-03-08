4217b0c80da7fa573e98a13fa85212a54ea85c03
Advent <49820054+DarkAdvent@users.noreply.github.com>
2023-03-07 18:07:09 -0600

    Added Olwen/Bharat

--- text_dialogues/Chapter20FCDialogue.asm
+++ text_dialogues/Chapter20FCDialogue.asm
@@ -11,3 +11,185 @@ ch20fc
 .byte WaitForA
 
 .byte EndText
+
+olwenbharat
+
+.byte left_slot
+.word LoadPortrait
+.word olwen_portrait 
+
+.byte right_slot
+.word LoadPortrait
+.word bharat_portrait
+
+.byte left_slot
+.text "Ah... Lord Bharat."
+.byte WaitForA 
+
+.byte right_slot
+.text "Hold!"
+.byte WaitForA 
+.byte NewLine
+.text "...Lady Olwen?!"
+.byte NewLine
+.text "Is that you?!"
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "In the flesh."
+.byte NewLine
+.text "I'm glad to have a moment"
+.byte NewLine
+.text "to exchange words with you."
+.byte WaitForA 
+
+.byte right_slot
+.byte ScrollText
+.text "As am I."
+.byte WaitForA 
+.byte NewLine
+.text "So the rumors are sadly true..."
+.byte NewLine
+.text "You're with the rebels now, correct?"
+.byte WaitForA 
+.byte ScrollText
+.text "I'm glad to see you unharmed, at least."
+.byte NewLine
+.text "I spoke with Reinhardt not too long ago."
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "My brother?!"
+.byte NewLine
+.text "How... How is he faring?"
+.byte WaitForA 
+
+.byte right_slot
+.byte ScrollText
+.text "He's deeply worried about you,"
+.byte NewLine
+.text "but I suppose you already knew that."
+.byte WaitForA 
+.byte ScrollText
+.text "He made an uproar once he heard the news."
+.byte WaitForA 
+.byte NewLine
+.text "He thinks you've were captured, maybe even"
+.byte NewLine
+.text "tortured by the rebels, and rallied the troops"
+.byte NewLine
+.text "to stand against such injustice."
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "Oh, brother..."
+.byte WaitForA 
+
+.byte right_slot
+.byte ScrollText
+.text "But here I see you, alive and well,"
+.byte NewLine
+.text "and rather complacent, if I may add."
+.byte NewLine
+.text "So let me ask you: what on earth happened?"
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "Are my reasons not clear?"
+.byte WaitForA
+.byte NewLine
+.text "These horrid Child Hunts rage on all across"
+.byte NewLine
+.text "the continent, and all of us back home have"
+.byte NewLine
+.text "allowed it to happen!"
+.byte WaitForA 
+
+.byte right_slot
+.byte ScrollText
+.text "I get where you're coming from, Olwen..."
+.byte NewLine
+.text "But let me tell you that King Bloom does not"
+.byte NewLine
+.text "take pleasure in them any more than you or I."
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "Hmph."
+.byte NewLine
+.text "If you truly knew where I'm coming from,"
+.byte NewLine
+.text "you would have done the same as I."
+.byte WaitForA 
+.byte ScrollText
+.text "The Liberation Army consists of more"
+.byte NewLine
+.text "than just the Knights of Leonster—-"
+.byte WaitForA 
+.byte ScrollText
+.text "People from all over the continent believe"
+.byte NewLine
+.text "in the righteousness of our cause! Do you really"
+.byte NewLine
+.text "wish to die supporting such cruel deeds?!"
+.byte WaitForA 
+
+.byte right_slot
+.byte ScrollText
+.text "You're saying I should join the rebels?"
+.byte NewLine
+.text "Hah, what a good joke."
+.byte WaitForA 
+.byte ScrollText
+.text "I'm not a pure-blooded noble like you, Olwen."
+.byte NewLine
+.text "I am a High General of House Friege!"
+.byte NewLine
+.text "I have honor and a reputation to uphold!"
+.byte WaitForA 
+.byte ScrollText
+.text "It's very easy for you to say that—"
+.byte NewLine
+.text "No matter what happens, not even the"
+.byte NewLine
+.text "Emperor would disrespect your brother."
+.byte WaitForA 
+.byte ScrollText
+.text "But me? I still have my children to take care of"
+.byte NewLine
+.text "back home, and I dare not think what would"
+.byte NewLine
+.text "happen if the news spread of me turning coat."
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "I... B-But think about this well, Lord Bharat!"
+.byte NewLine
+.text "Would this really be what your children"
+.byte NewLine
+.text "would want from you?!"
+.byte WaitForA 
+
+.byte right_slot
+.byte ScrollText
+.text "I believe further talk is now pointless, milady."
+.byte WaitForA 
+.byte ScrollText
+.text "Raise your sword, for we are enemies."
+.byte NewLine
+.text "Let me see if you really have what it takes"
+.byte NewLine
+.text "to challenge your brother. Come forth!"
+.byte WaitForA 
+
+.byte left_slot
+.byte ScrollText
+.text "Lord Bharat?! I... No...!"
+.byte WaitForA 
+.byte EndText
