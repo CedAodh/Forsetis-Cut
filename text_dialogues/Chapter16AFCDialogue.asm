b5473213c2382ea7c9ed6606a3bb99a9843c695c
Advent <49820054+DarkAdvent@users.noreply.github.com>
2023-03-26 15:56:41 -0600

    Added Dean/Linoan A Convo

--- text_dialogues/Chapter16AFCDialogue.asm
+++ text_dialogues/Chapter16AFCDialogue.asm
@@ -21,4 +21,113 @@ chapter16aoltophopening
 	.byte WaitForA
 	
 	.byte EndText
-	
+
+
+deanlinoanA
+
+.byte right_slot
+.word LoadPortrait
+.word dean_portrait
+.text "Hm?"
+.byte NewLine
+.text "What's wrong, Linoan?"
+.byte WaitForA
+.byte NewLine
+.text "Linoan!"
+.byte WaitForA
+
+.byte left_slot
+.word LoadPortrait
+.word linoan_portrait
+.text "Ah!"
+.byte WaitForA
+.byte NewLine
+.text "Oh, Dean, it's just you..."
+.byte NewLine
+.text "I apologize, I was so lost in thought."
+.byte WaitForA
+.byte NewLine
+.text "You scared me!"
+.byte WaitForA
+
+.byte right_slot
+.byte ScrollText
+.text "Then it is I who should apologize."
+.byte NewLine
+.text "I just couldn't help but notice you were"
+.byte NewLine
+.text "looking rather downcast. Is everything okay?"
+.byte WaitForA
+
+.byte left_slot
+.byte ScrollText
+.text "Oh, how I wish I could hide things from you..."
+.byte NewLine
+.text "I still feel deep regret over all of"
+.byte NewLine
+.text "what happened back in Tarrah."
+.byte WaitForA
+.byte ScrollText
+.text "I can't fool myself into thinking this was"
+.byte NewLine
+.text "my choice. We didn't even stand a chance,"
+.byte NewLine
+.text "and all I could do was concede peacefully."
+.byte WaitForA
+
+.byte right_slot
+.byte ScrollText
+.text "Look, Linoan."
+.byte NewLine
+.text "You did all that you could, and your subjects'"
+.byte NewLine
+.text "trust is what even allowed you to come so far."
+.byte WaitForA
+.byte ScrollText
+.text "You don't need to worry about them."
+.byte NewLine
+.text "I'm sure they understand this was for the best."
+.byte WaitForA
+.byte ScrollText
+.text "Thracia does not partake in the Child Hunts,"
+.byte NewLine
+.text "and I'm sure they will appreciate getting to"
+.byte NewLine
+.text "know their duchess's fiancé a little bit more."
+.byte WaitForA
+
+.byte left_slot
+.byte ScrollText
+.text "Oh, Dean... You're absolutely right."
+.byte NewLine
+.text "I'm sorry, it's not like me to doubt myself."
+.byte WaitForA
+.byte NewLine
+.text "I can't even remember what got me"
+.byte NewLine
+.text "so worked up in the first place!"
+.byte WaitForA
+
+.byte right_slot
+.byte ScrollText
+.text "Now, there's the smile I was looking for."
+.byte WaitForA
+
+.byte left_slot
+.byte ScrollText
+.text "Thank you, Dean. For everything."
+.byte WaitForA
+.byte ScrollText
+.text "Please don't allow me to take so much of your"
+.byte NewLine
+.text "time. I'm sure Leif's Army is in need of your" 
+.byte NewLine
+.text "strength elsewhere."
+.byte WaitForA
+
+.byte right_slot
+.byte ScrollText
+.text "As you wish, milady."
+.byte WaitForA
+
+.byte EndText
